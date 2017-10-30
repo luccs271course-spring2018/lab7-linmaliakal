663f8f3e5fe6bb1bc0c7f37346d27f9ad34eeb3c
linmaliakal <lmaliakal@luc.edu>
2017-10-30 01:30:11 +0000

    updated TODOs to synchronize with lab5
    Signed-off-by: linmaliakal <lmaliakal@luc.edu>

--- src/main/java/edu/luc/cs271/linkedstack/LinkedStack.java
+++ src/main/java/edu/luc/cs271/linkedstack/LinkedStack.java
@@ -2,6 +2,7 @@ package edu.luc.cs271.linkedstack;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 
 public class LinkedStack<E> implements IStack<E> {
@@ -9,36 +10,51 @@ public class LinkedStack<E> implements IStack<E> {
   /** The topmost node of this stack. The stack gets pushed down from here. */
   private Node<E> top;
 
-  // TODO why don't we need an explicit constructor?
-
   @Override
   public E push(final E obj) {
-    // TODO
-    return null;
+    // DONE
+    top = new Node<>(obj, top);
+    return obj;
   }
 
   @Override
   public E peek() {
-    // TODO
-    return null;
+    // DONE
+    if (isEmpty()) {
+      throw new NoSuchElementException();
+    } else {
+    return top.data;
+    }
   }
 
   @Override
   public E pop() {
-    // TODO
-    return null;
+    // DONE
+    if (isEmpty()) {
+      throw new NoSuchElementException();
+    } else {
+      E result = top.data;
+      top = top.next;
+      return result; 
+    }
   }
 
   @Override
   public boolean isEmpty() {
-    // TODO
-    return false;
+    // DONE
+    return top == null;
   }
 
   @Override
   public List<E> asList() {
-    // TODO implement using an ArrayList preallocated with the right size
-    // TODO add any instance variable(s) required to support this
-    return null;
+    // DONE implement using an ArrayList preallocated with the right size
+    // DONE add any instance variable(s) required to support this
+    List<E> newList = new ArrayList<E>();
+    
+    while (top != null) {
+      newList.add(top.data);
+      top = top.next;
+    }
+    return newList;
   }
 }
