b8739ae612ac6ce7929031154ec7fc31ec874678
linmaliakal <lmaliakal@luc.edu>
2017-10-30 01:44:13 +0000

    updated ReverseLines, added TODO to complete printReverse method
    Signed-off-by: linmaliakal <lmaliakal@luc.edu>

--- src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
+++ src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
@@ -5,19 +5,13 @@ import java.util.Scanner;
 public class ReverseLines {
 
   public static void main(final String[] args) {
-    // TODO read successive input lines until EOF, then print out in reverse order
-
-
-
     final Scanner input = new Scanner(System.in);
-    while (input.hasNextLine()) {
-      final String line = input.nextLine();
-
-
-
-    }
-
-
+    printReverse(input);
 
   }
+  
+  private static void printReverse(final Scanner input) {
+    // TODO recursively read and print successive input lines until EOF, then print them out in reverse order
+  }
+  
 }
